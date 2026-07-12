7084821e6e0ef35a0f81d097a89202af3d45246b
abinauuu <abhinavsh2499@gmail.com>
2026-07-13 04:06:51 +0530

    added investment routes

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const userRoutes = require("./routes/userRoutes.js")
 app.use("/api/user",userRoutes)
 const propertyRoutes=require("./routes/propertyRoutes.js")
 app.use("/api/properties",propertyRoutes)
+const investmentroutes = require("./routes/investmentRoutes.js")
+app.use("/api/investments",investmentroutes)
 
 app.listen(process.env.PORT,()=>{
     console.log(`server is running at port ${process.env.PORT}`)
